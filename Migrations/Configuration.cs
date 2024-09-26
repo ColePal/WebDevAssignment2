3e163eac0c972a6412467b89cfd3fb23752ea409
Cole <colepalffy1@gmail.com>
2024-09-27 10:42:06 +1200

    Added Movies model and controller and database.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace FilmFriends.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FilmFriends.Data.FilmFriendsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "FilmFriends.Data.FilmFriendsContext";
+        }
+
+        protected override void Seed(FilmFriends.Data.FilmFriendsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FilmFriends.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public float? Budget { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Synopsis { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime RealeaseDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime AddedDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? UpdatedDate { get; set; }
+        public ICollection<string> Directors { get; set; }
+        public ICollection<string> Actors { get; set;}
+        public ICollection<string> Producers { get; set; }
+
+    }
+}
